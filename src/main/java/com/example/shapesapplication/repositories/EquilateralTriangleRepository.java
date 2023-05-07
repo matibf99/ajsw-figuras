946c12cf2cb781f80f8738f651ce357271ae8867
matibf99 <matibf99@gmail.com>
2023-05-06 21:34:40 -0300

    deleted unnecessary @Query annotations

--- src/main/java/com/example/shapesapplication/repositories/EquilateralTriangleRepository.java
+++ src/main/java/com/example/shapesapplication/repositories/EquilateralTriangleRepository.java
@@ -2,13 +2,11 @@ package com.example.shapesapplication.repositories;
 
 import com.example.shapesapplication.models.EquilateralTriangle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface EquilateralTriangleRepository extends JpaRepository<EquilateralTriangle, Long> {
-    @Query("SELECT t FROM EquilateralTriangle t WHERE t.color = :color")
     List<EquilateralTriangle> findAllByColor(String color);
 }
--- src/main/java/com/example/shapesapplication/repositories/RectangleRepository.java
+++ src/main/java/com/example/shapesapplication/repositories/RectangleRepository.java
@@ -2,13 +2,11 @@ package com.example.shapesapplication.repositories;
 
 import com.example.shapesapplication.models.Rectangle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface RectangleRepository extends JpaRepository<Rectangle, Long> {
-    @Query("SELECT r FROM Rectangle r WHERE r.color = :color")
     List<Rectangle> findAllByColor(String color);
 }
